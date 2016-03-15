526ce639265da9ac0c600d89edb8e9665222707f
pletzera <pletzera@niwa-1007638.niwa.local>
2016-03-16 09:04:36 +1300

    new results

--- results/scaling.py
+++ results/scaling.py
@@ -1,10 +1,11 @@
 from matplotlib import pylab
 
 nprocs = [32, 16, 8, 4, 2, 1]
-times = [6*60+43., 13*60+27., 29*60+28., 41*60+31., 48*60+31., 95*60+19.]
+times = [6*60+43., 13*60+27., 22*60+4., 31*60+27., 61*60+55., 95*60+21.]
 speedups = [times[-1]/times[i] for i in range(len(nprocs))]
 
 pylab.plot(nprocs, speedups, nprocs, speedups, 'kx', nprocs, nprocs, 'r--')
-pylab.title('QuakeCORE speedup (FitzRoy)')
+pylab.title('Emod3d LPSim-2010Dec26_m4.7pt_v1_Cant1D_v1-nz01-h0.500_V3.04 (FitzRoy)')
 pylab.xlabel('nprocs')
+pylab.ylabel('speedup')
 pylab.show()
